fa2134e83abfd6fbfddd7b8430f0abf464d21568
Christoph Schmied <smith1401@hotmail.com>
2024-01-03 09:51:14 +0100

    got rid of not working auto-tuner

--- src/frt/services/pid_tuner_svc.h
+++ src/frt/services/pid_tuner_svc.h
@@ -3,11 +3,11 @@
 
 #include <Arduino.h>
 #include <frt/frt.h>
-#include <smart_rice_cooker.h>
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #include <AsyncTCP.h>
 #include <sTune.h>
+#include <frt/services/output_control_svc.h>
 
 #define RECORD_PID_VALUES "pid_values"
 #define RECORD_TEMPERATURE "temperature"
@@ -20,6 +20,12 @@ struct PIDValues
     float d;
 };
 
+union Float
+{
+    float f;
+    uint8_t bytes[4];
+};
+
 namespace frt
 {
     class PIDTunerServiceTCP : public frt::Task<PIDTunerServiceTCP, 4096>
@@ -30,37 +36,37 @@ namespace frt
         bool run() override;
 
     private:
-        Publisher<msgs::PID> *_pid_pub;
+        Publisher<OutputPower> *_output_pub;
         Subscriber<msgs::Temperature> *_input_sub;
         AsyncServer *_server;
         AsyncClient *_client;
     };
 
-    class PIDAutoTunerService : public frt::Task<PIDAutoTunerService>
-    {
-    public:
-        PIDAutoTunerService();
-        virtual ~PIDAutoTunerService();
-        bool run() override;
+    // class PIDAutoTunerService : public frt::Task<PIDAutoTunerService>
+    // {
+    // public:
+    //     PIDAutoTunerService();
+    //     virtual ~PIDAutoTunerService();
+    //     bool run() override;
 
-    private:
-        Publisher<msgs::PID> *_pid_pub;
-        Subscriber<msgs::Temperature> *_input_sub;
-        sTune *_tuner;
+    // private:
+    //     Publisher<msgs::PID> *_pid_pub;
+    //     Subscriber<msgs::Temperature> *_input_sub;
+    //     sTune *_tuner;
 
-        // user settings
-        uint32_t settleTimeSec = 10;
-        uint32_t testTimeSec = 500; // runPid interval = testTimeSec / samples
-        const uint16_t samples = 500;
-        const float inputSpan = 200;
-        const float outputSpan = 1000;
-        float outputStart = 0;
-        float outputStep = 50;
-        float tempLimit = 150;
-        uint8_t debounce = 1;
+    //     // user settings
+    //     uint32_t settleTimeSec = 10;
+    //     uint32_t testTimeSec = 500; // runPid interval = testTimeSec / samples
+    //     const uint16_t samples = 500;
+    //     const float inputSpan = 200;
+    //     const float outputSpan = 1000;
+    //     float outputStart = 0;
+    //     float outputStep = 50;
+    //     float tempLimit = 150;
+    //     uint8_t debounce = 1;
 
-        float Input, Output, Setpoint = 80, Kp, Ki, Kd;
-    };
+    //     float Input, Output, Setpoint = 80, Kp, Ki, Kd;
+    // };
 }
 
 #endif // __PID_TUNER_SVC_H__
